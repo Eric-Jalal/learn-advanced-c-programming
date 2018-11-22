66916266dffc0e74ff6256833da92c3199c2c180
Amir <amirhossein.hosseini@smarp.com>
2018-11-22 12:40:30 +0200

    Exception basics

---
+++ Exception Basics/cmake-build-debug/CMakeFiles/Exception_Basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exception_Basics.dir/main.cpp.o"
+  "Exception_Basics.pdb"
+  "Exception_Basics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exception_Basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
